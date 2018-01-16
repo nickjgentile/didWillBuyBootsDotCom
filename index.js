b0b7ff42b1340eaaae3e4ee510146df65bf8672d
Nick Gentile <nickjgentile@gmail.com>
2018-01-16 17:44:20 -0600

    more dependencies, file structure continued

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.use(express.static(__dirname + '/public'));
 
 const port = process.env.PORT || 3030;
 
-app.get('/*', function(req, res) {
-    res.render('index');
-    //THIS IS GIVING AN ERROR BECAUSE IT HAS NO ENGINE SET > NEED TO FIX THIS
-})
+// app.get('/*', function(req, res) {
+//     res.render('index');
+//     //THIS IS GIVING AN ERROR BECAUSE IT HAS NO ENGINE SET > NEED TO FIX THIS
+// })
 
 app.listen(port, function() {
     console.log("Listening on " + port);
-});
+})
---
+++ src/app.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // BINDINGS HERE
+
+        //STATE HERE
+        this.state = {
+            propOne: null
+        };
+    }
+
+    // FUNCTIONS and VARS HERE 
+
+    render() {
+        return (
+            <div>
+                Yo Bitch
+            </div>
+        );
+    }
+}
+
+export default App;

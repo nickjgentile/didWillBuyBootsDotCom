525dae67f93b0f63815f946ecace206ae0f54677
Nick Gentile <nickjgentile@gmail.com>
2018-02-01 15:52:38 -0600

    next step: figure out css lib to use

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Header extends React.Component {
+    constructor(props){
+        super(props);
+
+        //Bindings here
+
+        //state here
+    }
+
+        //methods here
+
+        //render here
+        render() {
+            return(
+                <div>
+                    Bye Mf
+                </div>
+            )
+        }
+}
+
+export default Header;
